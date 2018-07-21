cff9c7f08375cb51d874183c09d97c3347fcce34
bobilev <l7upoy@gmail.com>
2018-07-21 10:32:21 +0300

    fix port(del localhost)

--- front/source/js/util.js
+++ front/source/js/util.js
@@ -1,6 +1,6 @@
 'use strict'
 async function FecthApiPOST(apiMap) {
-  let request = `http://localhost/api`
+  let request = `/api`
   console.log("newfecthapi apiMap json", JSON.stringify(apiMap))
   return fetch(request,{
     method: "POST",
